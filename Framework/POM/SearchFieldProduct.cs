322c166247766012dd85c8274345be83daaf6d68
AruneSR <missarune@gmail.com>
2023-03-08 21:44:17 +0200

    2 test Case passed

---
+++ Framework/POM/SearchFieldProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.POM
+{
+    public class SearchFieldProduct
+    {
+        public static void ClickButtonSearch()
+        {
+            string locator = "//*[@class='yp-header-desktop']//*[@id='small-search-box-form']/button";
+            Common.ClickElement(locator);
+
+
+
+        }
+
+        public static void EntervalueinputSearch(string valueinputSearch)
+        {
+            string locator = "//*[@class='yp-header-desktop']//*[@id='small-searchterms']";
+            Common.SendKeys(valueinputSearch, locator);
+
+        }            
+    }
+
+}
---
+++ Tests/SearchFieldProductsTests.cs
@@ -0,0 +1,27 @@
+﻿using Framework.POM;
+using Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class SearchFieldProductsTests
+    {
+
+        [Test]
+        public void SearchFieldProducts()
+        {
+            string valueinputSearch = "pavadeliai";
+            Driver.SetupDriver();
+            Driver.OpenUrl("https://mumbo.lt/");
+            MumboPage.ClickCookiesAllaccept();
+            SearchFieldProduct.EntervalueinputSearch(valueinputSearch);
+            SearchFieldProduct.ClickButtonSearch();
+            Driver.QuitDriver();
+        }
+    }
+}
